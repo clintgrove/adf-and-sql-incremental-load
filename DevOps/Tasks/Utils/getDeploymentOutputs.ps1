f2d4c869043a462e06b7c4a594f2e214fe56fe0e
Clint Grove <clintelyonson@gmail.com>
2023-10-06 10:32:10 +0100

    passthtr

--- DevOps/Tasks/Utils/getDeploymentOutputs.ps1
+++ DevOps/Tasks/Utils/getDeploymentOutputs.ps1
@@ -1,14 +1,20 @@
 param(
- [string]  $resourceGroupName
+[String] [Parameter(Mandatory = $true)] $resourceGroupName
 )
-Import-Module -Name Az.Resources
 
-$resourceGroupName = 'lz-ghub-test-uks-rg-01'
+#https://stackoverflow.com/questions/36948549/how-do-i-use-arm-outputs-values-another-release-task
+
+#$resourceGroupName = 'lz-ghub-test-uks-rg-01'
 $lastDeployment = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName | Sort Timestamp -Descending | Select -First 1 
-$deploymentOutputs = $lastDeployment.Outputs
 
-# $deploymentOutputs.GetEnumerator() | % {
-#     $_.Value.Value
+if(!$lastDeployment) {
+    throw "Deployment could not be found for Resource Group '$resourceGroupName'."
+}
 
+if(!$lastDeployment.Outputs) {
+    throw "No output parameters could be found for the last deployment of Resource Group '$resourceGroupName'."
+}
 
-# }
+$sqlSrvNameOutput = $lastDeployment.Outputs.armOutput.Value
+Write-Host "##vso[task.setvariable variable=sqlSrvNameOutput;isOutput=true]$sqlSrvNameOutput"
+Write-Host "The sqlServerName variable is $sqlSrvNameOutput"
