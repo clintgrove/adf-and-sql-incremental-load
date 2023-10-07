bb91d9de712ae48836a2a9965d295cac03c0c7ae
Clint Grove <clintelyonson@gmail.com>
2023-10-07 09:40:59 +0100

    puttin in ps1

--- DevOps/Tasks/Utils/getDeploymentOutputs.ps1
+++ DevOps/Tasks/Utils/getDeploymentOutputs.ps1
@@ -23,7 +23,7 @@ foreach ($key in $lastDeployment.Outputs.Keys){
         Write-Host "##vso[task.setvariable variable=$key;issecret=true]$value" 
     }
     else {
-        Write-Host "##vso[task.setvariable variable=sqlSrvNameOutput;]$value"
+        Write-Host "##vso[task.setvariable variable=sqlSrvNameOutput;isOutput=true]$value"
         #Write-Host "##vso[task.setvariable variable=$key;]$value"  
     }
 }
