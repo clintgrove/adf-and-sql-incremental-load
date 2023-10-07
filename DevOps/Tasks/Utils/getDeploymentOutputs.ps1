259f88ea2c3e4d3acc184bb9370e2546bea66645
Clint Grove <clintelyonson@gmail.com>
2023-10-07 08:29:14 +0100

    try var output

--- DevOps/Tasks/Utils/getDeploymentOutputs.ps1
+++ DevOps/Tasks/Utils/getDeploymentOutputs.ps1
@@ -5,7 +5,7 @@ param(
 #https://stackoverflow.com/questions/36948549/how-do-i-use-arm-outputs-values-another-release-task
 
 #$resourceGroupName = 'lz-ghub-test-uks-rg-01'
-$lastDeployment = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName | Sort Timestamp -Descending | Select -First 1 
+$lastDeployment = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName | Sort Timestamp -Descending | Where-Object {$_.DeploymentName -like "AzureSQL*"} | Select -First 1
 
 if(!$lastDeployment) {
     throw "Deployment could not be found for Resource Group '$resourceGroupName'."
@@ -15,6 +15,16 @@ if(!$lastDeployment.Outputs) {
     throw "No output parameters could be found for the last deployment of Resource Group '$resourceGroupName'."
 }
 
-$sqlSrvNameOutput = $lastDeployment.Outputs.armOutput.Value
-Write-Host "##vso[task.setvariable variable=sqlSrvNameOutput;isOutput=true]$sqlSrvNameOutput"
-Write-Host "The sqlServerName variable is $sqlSrvNameOutput"
+foreach ($key in $lastDeployment.Outputs.Keys){
+    $type = $lastDeployment.Outputs.$key.Type
+    $value = $lastDeployment.Outputs.$key.Value
+
+    if ($type -eq "SecureString") {
+        Write-Host "##vso[task.setvariable variable=$key;issecret=true]$value" 
+    }
+    else {
+        Write-Host "##vso[task.setvariable variable=sqlSrvNameOutput;]$value"
+        #Write-Host "##vso[task.setvariable variable=$key;]$value"  
+    }
+}
+
