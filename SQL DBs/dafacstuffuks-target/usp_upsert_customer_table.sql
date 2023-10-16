60510d35d6e5ab92a9a228df035e7d20615ed325
Clint Grove <clintelyonson@gmail.com>
2023-10-16 09:49:35 +0100

    added in sprocs in target database

---
+++ SQL DBs/dafacstuffuks-target/usp_upsert_customer_table.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[usp_upsert_customer_table] 
+AS
+
+BEGIN
+  MERGE customer_table AS target
+  USING staging.customer_table AS source
+  ON (target.PersonID = source.PersonID)
+  WHEN MATCHED THEN
+      UPDATE SET Name = source.Name,LastModifytime = source.LastModifytime
+  WHEN NOT MATCHED THEN
+      INSERT (PersonID, Name, LastModifytime)
+      VALUES (source.PersonID, source.Name, source.LastModifytime);
+END
---
+++ SQL DBs/dafacstuffuks-target/usp_upsert_project_table.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[usp_upsert_project_table]
+AS
+
+BEGIN
+  MERGE project_table AS target
+  USING staging.project_table AS source
+  ON (target.Project = source.Project)
+  WHEN MATCHED THEN
+      UPDATE SET Creationtime = source.Creationtime
+  WHEN NOT MATCHED THEN
+      INSERT (Project, Creationtime)
+      VALUES (source.Project, source.Creationtime);
+END
