0654a49ceedcf230bd5a27c8ae864c183b06da88
Clint Grove <clintelyonson@gmail.com>
2023-10-18 10:33:23 +0100

    adf output name

--- DevOps/Tasks/Utils/getDeploymentOutputs.ps1
+++ DevOps/Tasks/Utils/getDeploymentOutputs.ps1
@@ -5,19 +5,19 @@ param(
 #https://stackoverflow.com/questions/36948549/how-do-i-use-arm-outputs-values-another-release-task
 
 #$resourceGroupName = 'lz-ghub-test-uks-rg-01'
-$lastDeployment = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName | Sort Timestamp -Descending | Where-Object {$_.DeploymentName -like "AzureSQL*"} | Select -First 1
+$lastSQLDeployment = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName | Sort Timestamp -Descending | Where-Object {$_.DeploymentName -like "AzureSQL*"} | Select -First 1
 
-if(!$lastDeployment) {
+if(!$lastSQLDeployment) {
     throw "Deployment could not be found for Resource Group '$resourceGroupName'."
 }
 
-if(!$lastDeployment.Outputs) {
+if(!$lastSQLDeployment.Outputs) {
     throw "No output parameters could be found for the last deployment of Resource Group '$resourceGroupName'."
 }
 
-foreach ($key in $lastDeployment.Outputs.Keys){
-    $type = $lastDeployment.Outputs.$key.Type
-    $value = $lastDeployment.Outputs.$key.Value
+foreach ($key in $lastSQLDeployment.Outputs.Keys){
+    $type = $lastSQLDeployment.Outputs.$key.Type
+    $value = $lastSQLDeployment.Outputs.$key.Value
 
     if ($type -eq "SecureString") {
         Write-Host "##vso[task.setvariable variable=$key;issecret=true]$value" 
@@ -25,9 +25,32 @@ foreach ($key in $lastDeployment.Outputs.Keys){
     else {
         if ($key -eq "armOutput") {
             Write-Host "##vso[task.setvariable variable=sqlSrvNameOutput;isOutput=true]$value"
+        } 
+    }
+}
+
+$lastADFDeployment = Get-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName | Sort Timestamp -Descending | Where-Object {$_.DeploymentName -like "AzureDataFac*"} | Select -First 1
+
+if(!$lastADFDeployment) {
+    throw "Deployment could not be found for Resource Group '$resourceGroupName'."
+}
+
+if(!$lastADFDeployment.Outputs) {
+    throw "No output parameters could be found for the last deployment of Resource Group '$resourceGroupName'."
+}
+
+foreach ($key in $lastADFDeployment.Outputs.Keys){
+    $type = $lastADFDeployment.Outputs.$key.Type
+    $value = $lastADFDeployment.Outputs.$key.Value
+
+    if ($type -eq "SecureString") {
+        Write-Host "##vso[task.setvariable variable=$key;issecret=true]$value" 
+    }
+    else {
+        if ($key -eq "adfNameOutput") {
+            Write-Host "##vso[task.setvariable variable=adfNameOutput;isOutput=true]$value"
         }
-        #Write-Host "##vso[task.setvariable variable=sqlSrvNameOutput;isOutput=true]$value"
-        #Write-Host "##vso[task.setvariable variable=$key;]$value"  
+
     }
 }
 
