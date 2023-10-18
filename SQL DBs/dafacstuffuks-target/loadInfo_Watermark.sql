36f4a74016da52a00c78f95ce1ba90d4aefac083
Clint Grove <clintelyonson@gmail.com>
2023-10-18 11:10:25 +0100

    adding in load info watermark table

---
+++ SQL DBs/dafacstuffuks-target/loadInfo_Watermark.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[loadInfo_Watermark](
+	[rowNum] [int] IDENTITY(1,1) NOT NULL,
+	[dateofload] [datetime] NULL,
+	[sourceinfo] [varchar](50) NULL,
+	[targetinfo] [varchar](50) NULL
+) 
