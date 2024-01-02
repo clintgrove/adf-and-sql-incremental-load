c6c187090cb07696aaf8ec2e76bcf8aa69da124a
Clint Grove <clintelyonson@gmail.com>
2024-01-02 12:13:52 +0000

    adding some data to source tables

---
+++ SQL DBs/dafacstuffuks/Script.PostDeployment1.sql
@@ -0,0 +1,17 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+INSERT INTO [customer_table]
+VALUES(1,'Joe Reegsblog','2024-01-01')
+  
+INSERT INTO project_table 
+VALUES ('Amazing project','2024-01-01')
