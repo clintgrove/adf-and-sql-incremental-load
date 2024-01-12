6fdae4db60ea64ed4d26d6f63f331edd8d0aa297
Clint Grove <clintelyonson@gmail.com>
2024-01-12 14:43:21 +0000

    try catch

--- DevOps/Tasks/Utils/AddADFUserToSecurityGroup.ps1
+++ DevOps/Tasks/Utils/AddADFUserToSecurityGroup.ps1
@@ -1,8 +1,8 @@
 param (
     #[Parameter(Mandatory=$true)]
-    [String] $AD_SecurityGroup ,
+    [String] $AD_SecurityGroup = 'SQL-DataWriters-DEV',
     #[Parameter(Mandatory=$true)] 
-    [String] $FactoryNameUpToEnv
+    [String] $FactoryNameUpToEnv = 'adf-rcv6lib6rdt2c-uksouth-dev'
 )
 
 $idOfSecGroup = (Get-AzADGroup -DisplayName $AD_SecurityGroup).id
@@ -10,6 +10,14 @@ write-host $idOfSecGroup
 $idOfFactory = (Get-AzADServicePrincipal -DisplayNameBeginsWith $FactoryNameUpToEnv).id
 write-host $idOfFactory
 
-Add-AzADGroupMember -TargetGroupObjectId $idOfSecGroup -MemberObjectId $idOfFactory
-
-
+try {
+    Add-AzADGroupMember -TargetGroupObjectId $idOfSecGroup -MemberObjectId $idOfFactory -ErrorAction SilentlyContinue
+}
+catch {
+    if ($_.Exception.Message -like "*One or more added object references already exist for the following modified properties: 'members'*") {
+        Write-Host "Member already exists in the group, continuing..."
+    }
+    else {
+        throw
+    }
+}
