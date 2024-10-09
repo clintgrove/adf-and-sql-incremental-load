aef0826e8a1e5f5e49fe26a68c880e34bc96f50c
Clint Grove <clintelyonson@gmail.com>
2024-10-09 14:24:45 +0100

    making sourceinfo not null

--- SQL DBs/dafacstuffuks-target/loadInfo_Watermark.sql
+++ SQL DBs/dafacstuffuks-target/loadInfo_Watermark.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[loadInfo_Watermark](
 	[rowNum] [int] IDENTITY(1,1) NOT NULL,
 	[dateofload] [datetime] NOT NULL,
-	[sourceinfo] [varchar](50) NULL,
+	[sourceinfo] [varchar](50) NOT NULL,
 	[targetinfo] [varchar](50) NULL
 ) 
